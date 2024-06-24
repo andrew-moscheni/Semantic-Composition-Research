bd5a0fb7833777d1badaa617d9090b57ccf79746
armoredmenace <agmoscheni@gmail.com>
2024-06-23 21:18:54 -0400

    created a function to rotate proxies-TO BE DEBUGGED, plus some minor changes to fetch_html and seconds_conversion

--- html_downloader.py
+++ html_downloader.py
@@ -9,13 +9,23 @@ import requests
 import pickle
 import random
 import time
+import json
+import urllib
 
 from concurrent.futures import ThreadPoolExecutor
+from itertools import cycle
+from bs4 import BeautifulSoup
 
 
 # GLOBAL VARIABLES
 MAX_PAGES_PER_MIN = 5
 MAX_PAGES_BEFORE_RESET = 20
+proxy_sites = [
+    'https://free-proxy-list.net/uk-proxy.html',
+    'https://www.sslproxies.org/',
+    'https://www.us-proxy.org/',
+    'https://free-proxy-list.net/'
+]
 url_stems = [
     'https://merriam-webster.com/dictionary/',                   # MW Dictionary
     'https://www.dictionary.com/browse/',                        # Dictionary.com
@@ -24,7 +34,30 @@ url_stems = [
     'https://api.dictionaryapi.dev/api/v2/entries/en/'           # Free Online Dictionary API
 ]
 directory_names = ['mw', 'dict_dot_com', 'cambridge', 'online_api', 'collins']
+
+# make the word list and the list of working proxies (to be used later)
+def generate_proxies():
+    proxy_list = []
+    for site in proxy_sites:
+        response = requests.get(site)
+        soup = BeautifulSoup(response.text)
+        # go through the rows of the table
+        for row in soup.select('.table.table-striped.table-bordered tbody tr'):
+            # get the proxy
+            ip, port = row.select('td')[0:1]
+            schematic = 'https' if row.select('td')[6] == 'yes' else 'http'
+            proxy={schematic:schematic+'://'+ip+port}
+            # test it
+            try:
+                urllib.urlopen('https://www.nhl.com/', proxies=proxy)
+            except IOError:
+                continue
+            else:
+                proxy_list.append(proxy)
+    return proxy_list
+
 word_list = open('./oxford_3000/oxford_words.txt', 'r').read().split('\n')
+proxy_cycle = cycle(generate_proxies())
 
 
 # declare fetch_html function (to be used by multiple threads)
@@ -41,43 +74,24 @@ def fetch_html(index):
         directory = './'+directory_names[index]+'_pages'
         filename = word + '.pickle' if is_not_api else word + '.json'
         mode = 'wb' if is_not_api else 'w'
+        # cycle through the proxy list
+        proxy = next(proxy_cycle)
 
-        response = requests.get(url)
+        response = requests.get(url, proxies=proxy)
         if response.status_code > 229:
             raise RuntimeError(f'HTTP {response.status_code} Error.')
 
         raw_data = response.text if is_not_api else response.json()
         with open(directory + '/' + filename, mode) as file:
             pickle.dump(raw_data, file) if is_not_api else json.dump(raw_data, file)
-
-        # randomize request time
-        page_counter_before_reset += 1
-        if page_counter_before_reset >= MAX_PAGES_PER_MIN:
-            time.sleep(60-time_used_before_reset)
-            page_counter_before_reset, time_used_before_reset = 0, 0
-        elif total_pages_before_reset >= MAX_PAGES_BEFORE_RESET and total_time_used_before_reset <= 240:
-            time.sleep(240-total_time_used_before_reset)
-            page_counter_before_reset, time_used_before_reset = 0, 0
-            total_pages_before_reset, total_time_used_before_reset = 0, 0
-        else:
-            sleep_time = random.randint(8,10)
-            time.sleep(sleep_time)
-            total_time_used_before_reset += sleep_time
-            time_used_before_reset += sleep_time
     print(f'Finished processing {directory_names[index]}!')
 
 
-# pretty printing -- converts seconds to days, hours, minutes, seconds
+# pretty printing -- converts seconds to hours, minutes, seconds
 def seconds_conversion(seconds):
-    seconds = seconds % (24 * 3600)
-    days = seconds // 86400
-    seconds %= 86400
-    hour = seconds // 3600
-    seconds %= 3600
-    minutes = seconds // 60
-    seconds %= 60
-     
-    return "%d:%02d:%02d:%02d" % (days, hour, minutes, seconds)
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return f'{h:d}:{m:02d}:{s:02d}'
 
 
 if __name__ == '__main__':
@@ -86,4 +100,4 @@ if __name__ == '__main__':
     with ThreadPoolExecutor(max_workers=len(url_stems)) as executor:
         executor.map(fetch_html, range(len(url_stems)))
     print(f'Execution completed successfully in {seconds_conversion(time.time()-t1)} \
-        (days: hours:minutes:seconds)')
+        (hours:minutes:seconds)')
