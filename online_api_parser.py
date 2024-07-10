6795e0b0fad2ce26ecf5b5c82160738ce85211a3
armoredmenace <agmoscheni@gmail.com>
2024-07-10 17:57:25 -0400

    parsed the JSON from the online API and dumped it into the CSV file

---
+++ online_api_parser.py
@@ -0,0 +1,24 @@
+# import any dependencies
+import json
+import pandas as pd
+import os
+
+# name of the directory the files are under
+directory = './raw_data/online_api_raw_data'
+
+# filter through every file and extract the information from the JSON file
+for filename in os.listdir(directory):
+    data = json.load(open(os.path.join(directory, filename)))
+
+    for d in data:
+        word = d['word']
+        pos = d['meanings'][0]['partOfSpeech']
+
+        for def_list in d['meanings'][0]['definitions']:
+            dfn = def_list['definition']
+            df = pd.DataFrame({
+                'word': [word],
+                'pos': [pos],
+                'dfn': [dfn]
+            })
+            df.to_csv('./oxford_3000_dfns.csv', mode='a', index=False, header=False)
